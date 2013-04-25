d35c8ef288b42c05919543b2800c2be4afeff50a
DucHa <ducha@DucHas-iMac.local>
2013-04-25 16:30:59 +0700

    a

---
+++ Denkeoquan/Denkeoquan/MyAppDelegate.h
@@ -0,0 +1,13 @@
+//
+//  MyAppDelegate.h
+//  Denkeoquan
+//
+//  Created by DucHa on 4/25/13.
+//  Copyright (c) 2013 DucHa. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MyAppDelegate : NSObject
+
+@end
---
+++ Denkeoquan/Denkeoquan/MyAppDelegate.m
@@ -0,0 +1,13 @@
+//
+//  MyAppDelegate.m
+//  Denkeoquan
+//
+//  Created by DucHa on 4/25/13.
+//  Copyright (c) 2013 DucHa. All rights reserved.
+//
+
+#import "MyAppDelegate.h"
+
+@implementation MyAppDelegate
+
+@end
