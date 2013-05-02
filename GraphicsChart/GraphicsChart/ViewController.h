2c2cc74e7f635fd72882435312f9999d34c8bcf5
DucHa <ducha@DucHas-iMac.local>
2013-05-02 16:41:07 +0700

    a

--- GraphicsChart/GraphicsChart/ViewController.h
+++ GraphicsChart/GraphicsChart/ViewController.h
@@ -7,7 +7,6 @@
 //
 
 #import <UIKit/UIKit.h>
-#import "DataManager.h"
 #import <AVFoundation/AVFoundation.h>
 
 @interface ViewController : UIViewController <UIWebViewDelegate>
--- GraphicsChart/GraphicsChart/ViewController.m
+++ GraphicsChart/GraphicsChart/ViewController.m
@@ -18,8 +18,6 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
-    DataManager *dataManager = [DataManager new];
-    [dataManager writeData];
     
     NSString *pathName = [[NSBundle mainBundle] pathForResource:@"index" ofType:@"html" inDirectory:@"/"];
     NSString *htmlString = [NSString stringWithContentsOfFile:pathName encoding:NSUTF8StringEncoding error:nil];
